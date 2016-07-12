bd986cbcd8f0da45ea5465593ac86d778d8d5a70
vladpliuta <vlad.pliuta@gmail.com>
2016-07-12 20:16:14 +0300

    remake StringCheckCyclic under conditions  - create method

--- homeTask3/StringCheckCyclic.java
+++ homeTask3/StringCheckCyclic.java
@@ -3,29 +3,38 @@ package homeTask3;
 *
 *@author Vladimir Pliuta
 */
-public class StringCheckCyclic {
-
-	public static void main(String[] args) {
-		String firstString = new String("erbottlewat");//input first text 
-		String secondString = new String("waterbottle");//input second text
+public static boolean isSubstring (String s1, String s2) { //create method
+		/* я бы конечно назвал бы firstString и secondString, 
+		 * ведь типа цифры в названии перевеменных это не красиво - 
+		 * но увы увы - таково условие задачи
+		 */
 		boolean resalt = false;
 		//check cyclic shift
-		if (firstString.length() == secondString.length()) {
-			for(int i = 0; i < firstString.length(); i++) {
-				String temp = "".concat(firstString.substring(i,firstString.length())).concat(firstString.substring(0,i)) ;
-				if (secondString.equals(temp)) {
+		if (s1.length() == s1.length()) {
+			for(int i = 0; i < s1.length(); i++) {
+				String temp = "".concat(s1.substring(i,s1.length())).concat(s1.substring(0,i)) ;
+				if (s2.equals(temp)) {
 					resalt = true;
 					break;
 				}
 			}
 		}
+		// output results
 		if (resalt) {
 			System.out.println ("Строка 2 получена циклическим сдвигом строки 1");
 		}
 		else {
 			System.out.println ("Строка 2 получена НЕ циклическим сдвигом строки 1");
 		}
-		//return resalt;
+		return resalt;
+	}
 	
+	
+	public static void main(String[] args) {
+		String firstString = new String("erbottlewat");//input first text 
+		String secondString = new String("waterbottle");//input second text
+		isSubstring(firstString,secondString);
+			
 	}	
 }
+
